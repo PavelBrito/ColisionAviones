6f9fb7dd070bfba3b3eb620a6647a869d36b4956
Pavel <you@example.com>
2025-05-06 23:36:44 -0500

    Estructura proyecto

---
+++ app/src/main/java/com/example/colisionaviones/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.colisionaviones;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listaAviones = findViewById(R.id.listaAviones);
+        btnNext = findViewById(R.id.btnNext);
+        plano =  Planificador.crearRutaInicial();
+        adapter = new AvionAdapter(plano.aviones);
+        listaAviones.setAdapter(adapter);
+        listaAviones.setLayoutManager(new GridLayoutManager(this, 5));
+
+        btnNext.setOnClickListener(v -> {
+            plano = plano.next();
+            adapter.notifyDataSetChanged();
+        });
+
+
+    }
+}
